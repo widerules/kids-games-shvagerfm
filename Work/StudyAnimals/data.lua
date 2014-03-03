1da3698b386df93029096a41970559bd334d042c
shvagerfm <shvagerfm@gmail.com>
2014-03-03 20:12:55 +0000

    study animals 0.1 commit

---
+++ Work/StudyAnimals/data.lua
@@ -0,0 +1,29 @@
+local data = {};
+
+data.animalsNames = {"Bunny", "Cat", "Cow", "Dog", "Mouse", "Sheep"};
+data.animalsDescriptions = {
+	"Bunny Rhyme", 
+	"Cat Rhyme", 
+	"Cow Rhyme", 
+	"Dog Rhyme", 
+	"Mouse Rhyme", 
+	"Sheep Rhyme"
+};
+data.animalsImages = {
+	"images\\bunny.png",
+	"images\\cat.png", 
+	"images\\cow.png",
+	"images\\dog.png", 
+	"images\\mouse.png", 
+	"images\\sheep.png"
+};
+data.foodsImages = {
+	"images\\carrot.png",
+	"images\\milk.png",
+	"images\\seno.png",
+	"images\\bone.png",
+	"images\\cheese.png",
+	"images\\grass.png"
+};
+
+return data;
