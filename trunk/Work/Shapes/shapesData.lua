0327a6162625781ac76a819c2b0823e51aaecd73
shvagerfm@gmail.com <shvagerfm@gmail.com@b6057328-8ef7-3bbf-bd54-756edf35c897>
2014-03-04 20:41:25 +0000

    first commit

---
+++ trunk/Work/Shapes/shapesData.lua
@@ -0,0 +1,32 @@
+local data = {};
+
+data.animals = 
+{
+	"bat",
+	"bear",
+	"beawer",
+	"bird",
+	"camel",
+	"cat",
+	"chiсken",
+	"coala",
+	"crocodile",
+	"deer",
+	"dog",
+	"duck",
+	"elefant",
+	"fox",
+	"hippo",
+	"lion",
+	"monkey",
+	"owl",
+	"pig",
+	"penguin",
+	"rabbit",
+	"shark",
+	"squirrel",
+	"tiger",
+	"whale"
+}
+
+return data;
