d715e27b6718873ce7ef563d32e3ef9a2c64a0f0
shvagerfm@gmail.com <shvagerfm@gmail.com@b6057328-8ef7-3bbf-bd54-756edf35c897>
2014-03-12 20:44:52 +0000

    first buggy commit

---
+++ trunk/Work/StudyColors/studyData.lua
@@ -0,0 +1,31 @@
+local constants = require( "constants")
+
+local data = {}
+
+data.butterfliesPath = "images/butterflies/"
+data.circlesPath = "images/circles/"
+data.format = ".png"
+
+data.colors = 
+{
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"cyan",
+	"blue",
+	"purple"	
+}
+
+data.textColors = 
+{
+	{1, 0, 0},
+	{1, 0.8, 0},
+	{1, 1, 0},
+	{0, 1, 0},
+	{0, 1, 1},
+	{0, 0, 1},
+	{1, 0, 1}
+}
+
+return data
