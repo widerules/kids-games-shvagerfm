df3c492d88eddfeb9cd816453aebcf4ec170ef11
shvagerfm <shvagerfm@gmail.com>
2014-03-14 16:30:56 +0000

    FIX!

--- Publish/AnimalsForKids/scenes/game4.lua
+++ Publish/AnimalsForKids/scenes/game4.lua
@@ -23,6 +23,7 @@ local sun
 local cloud
 local rain
 
+local group
 
 local layers = {}
 local groups = {}
@@ -40,298 +41,321 @@ local birdSound = audio.loadSound("sounds/birds_sing.ogg")
 local rainSound = audio.loadSound("sounds/rain_sound.ogg")
 local hogSound = audio.loadSound("sounds/hog_sound2.mp3")
 
+local timers = {}
+
 local function onNextButtonClicked()
-        storyboard.reloadScene( )
+	storyboard.reloadScene( )
 end
 
 local function onHomeButtonClicked()
-        storyboard.gotoScene("scenetemplate", "slideRight", 800)
-        storyboard.removeScene("scenes.game3")
+	storyboard.gotoScene("scenetemplate", "slideRight", 800)
+	-- storyboard.removeScene("scenes.game3")
 end
 
 local function showPopUp()
-        popupBg = display.newImage( "images/popupbg.png", constants.CENTERX, constants.CENTERY );
-        popupBg.height = 0.7*constants.H;
-        popupBg.width = 0.7*constants.W;
+	popupBg = display.newImage( "images/popupbg.png", constants.CENTERX, constants.CENTERY )
+	popupBg.height = 0.7*constants.H
+	popupBg.width = 0.7*constants.W
 
-        popupText = display.newText("Well done !", popupBg.x, 0, native.systemFont, 2*_FONTSIZE);
-        popupText.y = popupBg.y-popupBg.height+2*popupText.width/3;
+	popupText = display.newText("Well done !", popupBg.x, 0, native.systemFont, 2*_FONTSIZE)
+	popupText.y = popupBg.y-popupBg.height+2*popupText.width/3
 
-        homeBtn = widget.newButton
-        {
-                width = 0.4*popupBg.height,
-                height = 0.4*popupBg.height,
-                x = popupBg.x - 0.4*popupBg.width/2,
-                y = popupBg.y + 0.4*popupBg.height/2,
-                defaultFile = "images/home.png",
-                overFile = "images/homehover.png"
-        }
-        homeBtn:addEventListener( "tap", onHomeButtonClicked );
+	homeBtn = widget.newButton
+	{
+		width = 0.4*popupBg.height,
+		height = 0.4*popupBg.height,
+		x = popupBg.x - 0.4*popupBg.width/2,
+		y = popupBg.y + 0.4*popupBg.height/2,
+		defaultFile = "images/home.png",
+		overFile = "images/homehover.png"
+	}
+	homeBtn:addEventListener( "tap", onHomeButtonClicked )
 
-        nextBtn = widget.newButton
-        {
-                width = 0.4*popupBg.height,
-                height = 0.4*popupBg.height,
-                x = popupBg.x + 0.4*popupBg.width/2,
-                y = popupBg.y + 0.4*popupBg.height/2,
-                defaultFile = "images/next.png",
-                overFile = "images/next.png"
-        }       
-        nextBtn:addEventListener( "tap", onNextButtonClicked );
-end;
+	nextBtn = widget.newButton
+	{
+		width = 0.4*popupBg.height,
+		height = 0.4*popupBg.height,
+		x = popupBg.x + 0.4*popupBg.width/2,
+		y = popupBg.y + 0.4*popupBg.height/2,
+		defaultFile = "images/next.png",
+		overFile = "images/next.png"
+	}       
+	nextBtn:addEventListener( "tap", onNextButtonClicked )
+end
 
 local function onItemClicked(event)
-        local t = event.target
+	local t = event.target
 
-        local function updateScore ()
-                score.text = "Score: "..itemsFound
-        end
+	local function updateScore ()
+		score.text = "Score: "..itemsFound
+	end
 
-        local function vanishAway ()    
-                transition.to(t,{time = 1000, alpha = 0, x =constants.W, y = 0, xScale = 0.1, yScale = 0.1, onComplete = updateScore})
-        end     
-        transition.scaleTo(t, {xScale = 1.5*t.xScale, yScale = 1.5*t.yScale, time = 500, onComplete = vanishAway})
-        t:removeEventListener( "touch", onItemClicked ) 
+	local function vanishAway ()    
+		transition.to(t,{time = 1000, alpha = 0, x =constants.W, y = 0, xScale = 0.1, yScale = 0.1, onComplete = updateScore})
+	end
+	transition.scaleTo(t, {xScale = 1.5*t.xScale, yScale = 1.5*t.yScale, time = 500, onComplete = vanishAway})
+	t:removeEventListener( "touch", onItemClicked ) 
 
-        itemsFound = itemsFound + 1
-        
-        if (itemsFound == 7) then
-        	audio.play( soundHarp )
-                timer.performWithDelay( 800, showPopUp, 1)             
-        end
+	itemsFound = itemsFound + 1
+	
+	if (itemsFound == 7) then
+		audio.play( soundHarp )
+		timers[#timers+1] = timer.performWithDelay( 800, showPopUp, 1)             
+	end
 end
 
 local function fillWithHogs(group)
-        audio.play(hogSound)
-        for i = 1, 7, 1 do
-                local function addHog()
-                        hogs[i] = display.newImage (_HOGPATH, data.hogsPositions.x[i], data.hogsPositions.y[i])
-                        hogs[i].width = data.HOGIMAGESIZE*data.hogsSizes[i]
-                        hogs[i].height = data.HOGIMAGESIZE*data.hogsSizes[i]
-                        hogs[i].xScale = data.hogsScale[i]
-                        hogs[i]:addEventListener( "touch", onItemClicked )                      
-                        groups[data.hogsGroups[i]]:insert (hogs[i])                             
-                end
-                timer.performWithDelay( i*300, addHog )
-        end     
+	local i
+
+	audio.play(hogSound)
+
+	for i = 1, 7, 1 do
+		local function addHog()
+			hogs[i] = display.newImage (_HOGPATH, data.hogsPositions.x[i], data.hogsPositions.y[i])
+			hogs[i].width = data.HOGIMAGESIZE*data.hogsSizes[i]
+			hogs[i].height = data.HOGIMAGESIZE*data.hogsSizes[i]
+			hogs[i].xScale = data.hogsScale[i]
+			hogs[i]:addEventListener( "touch", onItemClicked )                      
+			groups[data.hogsGroups[i]]:insert (hogs[i])                             
+		end
+		timers[#timers + 1] = timer.performWithDelay( i*300, addHog )
+	end     
 end
 
 local function fillWithMushrooms(group)
-        for i = 1, 7, 1 do
-                local function addMushroom()
-                        mushrooms[i] = display.newImage (_MUSHPATH..data.mushroomsTypes[i].._FORMAT, data.mushroomsPositions.x[i], data.mushroomsPositions.y[i])
-                        mushrooms[i].width = data.MUSHIMAGESIZE*data.mushroomsSizes[i]
-                        mushrooms[i].height = data.MUSHIMAGESIZE*data.mushroomsSizes[i]
-                        mushrooms[i].xScale = data.mushroomsScale[i]
-                        mushrooms[i]:addEventListener( "touch", onItemClicked )
-                        groups[data.mushroomsGroups[i]]:insert(mushrooms[i])
-                end
-                timer.performWithDelay( i*300, addMushroom)
-
-        end
+	local i
+	for i = 1, 7, 1 do
+		local function addMushroom()
+			mushrooms[i] = display.newImage (_MUSHPATH..data.mushroomsTypes[i].._FORMAT, data.mushroomsPositions.x[i], data.mushroomsPositions.y[i])
+			mushrooms[i].width = data.MUSHIMAGESIZE*data.mushroomsSizes[i]
+			mushrooms[i].height = data.MUSHIMAGESIZE*data.mushroomsSizes[i]
+			mushrooms[i].xScale = data.mushroomsScale[i]
+			mushrooms[i]:addEventListener( "touch", onItemClicked )
+			groups[data.mushroomsGroups[i]]:insert(mushrooms[i])
+		end
+		timers[#timers + 1] = timer.performWithDelay( i*300, addMushroom)
+	end
 end
 
 local function fillWithBerries(group)
-        for i = 1, 7, 1 do
-                local function addBerry()
-                        berries[i] = display.newImage(_BERRYPATH, data.berriesPosition.x[i], data.berriesPosition.y[i])
-                        berries[i].width = data.BERRYIMAGESIZE * data.berriesSizes[i]
-                        berries[i].height = data.BERRYIMAGESIZE * data.berriesSizes[i]
-                        berries[i]:addEventListener( "touch", onItemClicked )
-                        groups[data.berriesGroups[i]]:insert(berries[i])
-                end
-                timer.performWithDelay( i*300, addBerry )
-        end
+	local i
+	for i = 1, 7, 1 do
+		local function addBerry()
+				berries[i] = display.newImage(_BERRYPATH, data.berriesPosition.x[i], data.berriesPosition.y[i])
+				berries[i].width = data.BERRYIMAGESIZE * data.berriesSizes[i]
+				berries[i].height = data.BERRYIMAGESIZE * data.berriesSizes[i]
+				berries[i]:addEventListener( "touch", onItemClicked )
+				groups[data.berriesGroups[i]]:insert(berries[i])
+		end
+		timers[#timers + 1] = timer.performWithDelay( i*300, addBerry )
+	end
 end
 
 local function sunAnimation ()
-        audio.play(birdSound)
+	audio.play(birdSound)
 
-        sun = display.newImage("images/sun.png", constants.W, constants.H)
-        sun.width = _IMAGESIZE 
-        sun.height = _IMAGESIZE
-        sun.anchorY = 0.4
-        groups[1]:insert(sun)
+	sun = display.newImage("images/sun.png", constants.W, constants.H)
+	sun.width = _IMAGESIZE 
+	sun.height = _IMAGESIZE
+	sun.anchorY = 0.4
+	-- groups[1]:insert(sun)
+	group:insert(sun)
+	local function fill ()
+		audio.stop()
+		timers[#timers + 1] = timer.performWithDelay( 500, fillWithHogs )
+	end
 
-        local function fill ()
-                audio.stop()
-                timer.performWithDelay( 500, fillWithHogs )
-        end
+	local function onRotateNeg()
+		transition.to(sun, {rotation = 0, time = 1000, onComplete = fill})              
+	end
 
-        local function onRotateNeg()
-                transition.to(sun, {rotation = 0, time = 1000, onComplete = fill})              
-        end
+	local function onRotatePos()            
+		transition.to(sun, {rotation = 360, time = 1000, onComplete = onRotateNeg})                     
+	end
 
-        local function onRotatePos()            
-                transition.to(sun, {rotation = 360, time = 1000, onComplete = onRotateNeg})                     
-        end
+	local function toNormal ()
+		transition.scaleTo(sun, {xScale = 1, yScale = 1, time = 500, onComplete = onRotatePos})
+	end
 
-        local function toNormal ()
-                transition.scaleTo(sun, {xScale = 1, yScale = 1, time = 500, onComplete = onRotatePos})
-        end
+	local function toBig()
+		transition.scaleTo(sun, {xScale = 1.5, yScale = 1.5, time = 500, onComplete = toNormal})
+	end
 
-        local function toBig()
-                
-                transition.scaleTo(sun, {xScale = 1.5, yScale = 1.5, time = 500, onComplete = toNormal})
-        end
-
-        transition.moveTo(sun, {x = constants.CENTERX, y = _IMAGESIZE*0.8, time = 2000, onComplete = toBig})
+	transition.moveTo(sun, {x = constants.CENTERX, y = _IMAGESIZE*0.8, time = 2000, onComplete = toBig})
 end
 
 local function onRainFinished(event)
-        if (event.phase == "ended") then
-                if (rain~=nil) then
-                        rain:removeSelf( )
-                        rain = nil
-                end
-                audio.pause( rainSound )
-                fillWithMushrooms()
-        end
+	if (event.phase == "ended") then
+		if (rain~=nil) then
+			rain:removeSelf( )
+			rain = nil
+		end
+		audio.pause( rainSound )
+		fillWithMushrooms()
+	end
 end
 
 
 local function rainAnimation()
-		audio.play( rainSound )
-        cloud = display.newImage("images/tucha.png", 0,0)
-        cloud.width = _IMAGESIZE
-        cloud.height = _IMAGESIZE
-        groups[1]:insert(cloud)
+	audio.play( rainSound )
+	cloud = display.newImage("images/tucha.png", 0,0)
+	cloud.width = _IMAGESIZE
+	cloud.height = _IMAGESIZE
+	-- groups[1]:insert(cloud)
+	group:insert(cloud)
 
-        local function rainStart()
+	local function rainStart()
+		local rainSheetData = 
+		{
+			width = constants.H,
+			height = 2*constants.H/3,
+			numFrames = 4,
+			sheetContentWidth = constants.H*2,
+			sheetContentHeight = 4*constants.H/3
+		}
+		local rainSheet = graphics.newImageSheet("images/rain.png", rainSheetData)
+		local sequenceDataRain = 
+		{
+			name = "rain",
+			start = 1,
+			count = 4,
+			time = 100,
+			loopCount=6,
+			loopDirection = forward
+		}
+		
+		rain = display.newSprite( rainSheet, sequenceDataRain)
+		group:insert(rain)
+		rain.x = constants.CENTERX
+		rain.y = constants.CENTERY
+		rain:addEventListener( "sprite", onRainFinished)
+		rain.timeScale = 0.4
+		rain:play()
 
-                local rainSheetData = 
-                {
-                width = constants.H,
-                height = 2*constants.H/3,
-                numFrames = 4,
-                sheetContentWidth = constants.H*2,
-                sheetContentHeight = 4*constants.H/3
-                }
-                local rainSheet = graphics.newImageSheet("images/rain.png", rainSheetData)
-        local sequenceDataRain = 
-        {
-                name = "rain",
-                start = 1,
-                count = 4,
-                time = 100,
-                loopCount=6,
-                loopDirection = forward
-        }
-        
-                rain = display.newSprite( rainSheet, sequenceDataRain)
-                rain.x = constants.CENTERX
-                rain.y = constants.CENTERY
-                rain:addEventListener( "sprite", onRainFinished)
-                rain.timeScale = 0.4
-                rain:play()
-
-
-        end
-        transition.moveTo(cloud, {x = constants.CENTERX, y = _IMAGESIZE*0.8, time = 2000, onComplete = rainStart})
+	end
+	transition.moveTo(cloud, {x = constants.CENTERX, y = _IMAGESIZE*0.8, time = 2000, onComplete = rainStart})
 end
 
 function scene:createScene (event)
+	group = self.view
+	local i
+
+	for i = 1, 10, 1 do
+		layers[i] = display.newImage("images/layer"..i..".png", constants.CENTERX, constants.CENTERY)
+		layers[i].height = constants.H
+		layers[i].width = constants.W
+	end     
+
+	for i = 1,9,1 do
+		groups[i] = display.newGroup( )
+	end
 	
-        local group = self.view
+	group:insert (1, layers[1])
+	group:insert (2, groups[1])
+	group:insert (3, layers[2])
+	group:insert (4, groups[2])
+	group:insert (5, layers[3])
+	group:insert (6, groups[3])
+	group:insert (7, layers[4])
+	group:insert (8, groups[4])
+	group:insert (9, layers[5])
+	
+	group:insert (10, groups[5])
+	group:insert (11, layers[6])
+	group:insert (12, groups[6])
+	group:insert (13, layers[7])
 
-        
-
-        for i = 1, 10, 1 do
-                layers[i] = display.newImage("images/layer"..i..".png", constants.CENTERX, constants.CENTERY)
-                layers[i].height = constants.H
-                layers[i].width = constants.W
-        end     
-
-        for i = 1,9,1 do
-                groups[i] = display.newGroup( )
-        end     
-
-        
-        group:insert (1, layers[1])
-        group:insert (2, groups[1])
-        group:insert (3, layers[2])
-        group:insert (4, groups[2])
-        group:insert (5, layers[3])
-        group:insert (6, groups[3])
-        group:insert (7, layers[4])
-        group:insert (8, groups[4])
-        group:insert (9, layers[5])
-        
-        group:insert (10, groups[5])
-        group:insert (11, layers[6])
-        group:insert (12, groups[6])
-        group:insert (13, layers[7])
-  
-        group:insert (14, groups[7])
-        group:insert (15, layers[8])
-        group:insert (16, groups[8])
-        group:insert (17, layers[9])
-        group:insert (18, groups[9])
-        group:insert (19, layers[10])
+	group:insert (14, groups[7])
+	group:insert (15, layers[8])
+	group:insert (16, groups[8])
+	group:insert (17, layers[9])
+	group:insert (18, groups[9])
+	group:insert (19, layers[10])
 end
-        
+		
 function scene:enterScene(event)
-	
-        local group = self.view
-        
-        itemsFound = 0
+	itemsFound = 0
 
-        score = display.newText("Score: 0", constants.W - _FONTSIZE/2, _FONTSIZE/2, native.systemFont, _FONTSIZE)
-        score.x = constants.W-score.width/2
-        if iteration == 2 then  
-                --TODO:Sun animation
-                sunAnimation()                  
-                iteration = iteration + 1               
-        elseif iteration == 1 then
-                --TODO: rain animation
-                rainAnimation()
-                --fillWithMushrooms(group)
-                iteration = iteration + 1
-        else
-                --TODO ???
-                fillWithBerries(group)
-                iteration = 1
-        end
+	score = display.newText("Score: 0", constants.W - _FONTSIZE/2, _FONTSIZE/2, native.systemFont, _FONTSIZE)
+	score.x = constants.W-score.width/2
+	group:insert(score)
+
+	if iteration == 2 then  
+		--TODO:Sun animation
+		sunAnimation()                  
+		iteration = iteration + 1               
+	elseif iteration == 1 then
+		--TODO: rain animation
+		rainAnimation()
+		--fillWithMushrooms(group)
+		iteration = iteration + 1
+	else
+		--TODO ???
+		fillWithBerries(group)
+		iteration = 1
+	end
 end
 
 function scene:exitScene(event)
+	 --      INSERT code here (e.g. stop timers, remove listeners, unload sounds, etc.)
+	audio.stop()
+	audio.dispose( rainSound )
+	rainSound = nil
+	audio.dispose(soundHarp)
+	soundHarp = nil
+	audio.dispose(birdSound)
+	birdSound = nil
+	audio.dispose(hogSound)
+	hogSound = nil
 
-        score:removeSelf( )
-        while (table.maxn(hogs)>0) do
-                hogs[#hogs]:removeSelf()
-                table.remove( hogs )
-        end
+	while (#timers > 0) do
+		timer.cancel(timers[#timers])
+		table.remove( timers )
+	end
 
-        while (table.maxn(mushrooms)>0) do
-                mushrooms[#mushrooms]:removeSelf()
-                table.remove(mushrooms)
-        end
+	transition.cancel()
+	
+	if (rain~=nil) then
+		rain:removeSelf( )
+		rain = nil
+	end
 
-        while (table.maxn(berries)>0) do
-                berries[#berries]:removeSelf()
-                table.remove(berries)
-        end
+	score:removeSelf( )
+	while (table.maxn(hogs)>0) do
+        hogs[#hogs]:removeSelf()
+        table.remove( hogs )
+	end
 
-        if (popupBg ~= nil) then
-                popupBg:removeSelf()
-                nextBtn:removeSelf()
-                homeBtn:removeSelf()
-                popupText:removeSelf()
-                popupBg = nil
-                nextBtn = nil
-                homeBtn = nil
-                popupText = nil
-        end
+	while (table.maxn(mushrooms)>0) do
+        mushrooms[#mushrooms]:removeSelf()
+        table.remove(mushrooms)
+	end
 
-        if (sun ~= nil) then
-                sun:removeSelf()
-                sun = nil
-        end
+	while (table.maxn(berries)>0) do
+        berries[#berries]:removeSelf()
+        table.remove(berries)
+	end
 
-        if (cloud ~= nil) then
-                cloud:removeSelf( )
-                cloud = nil
-        end
+	if (popupBg ~= nil) then
+        popupBg:removeSelf()
+        nextBtn:removeSelf()
+        homeBtn:removeSelf()
+        popupText:removeSelf()
+        popupBg = nil
+        nextBtn = nil
+        homeBtn = nil
+        popupText = nil
+	end
+
+	if (sun ~= nil) then
+        sun:removeSelf()
+        sun = nil
+	end
+
+	if (cloud ~= nil) then
+        cloud:removeSelf( )
+        cloud = nil
+	end
 end
 
 function scene:destroyScene(event)
