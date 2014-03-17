764e5295c89a28a23e9ac89eb68546fa90ed95e5
shvagerfm <shvagerfm@gmail.com>
2014-03-17 22:24:09 +0000

    Улучшена структура файлов проектов до уровня БОГА!

---
+++ Publish/English/AnimalsForKids/admob.lua
@@ -0,0 +1,40 @@
+admob = {}
+
+admob.provider = "admob"
+
+admob.countBeforeShowAds = 0
+
+
+local ads = require ( "ads" )
+
+-- Your application ID in admob
+
+admob.appID = "ca-app-pub-8763317495638154/6195451628"
+
+admob.init = function ()
+	if admob.appID then
+		ads.init( admob.provider, admob.appID, admob.adListener )
+	end
+end
+
+admob.showAd = function( adType )
+	if admob.countBeforeShowAds == 0 then
+    	ads.show( adType, { x=0, y=0 } )
+    	admob.countBeforeShowAds = 3
+    else
+    	admob.countBeforeShowAds = admob.countBeforeShowAds - 1
+	end
+end
+
+-- Set up ad listener.
+admob.adListener = function ( event )
+	-- event table includes:
+	-- 		event.provider
+	--		event.isError (e.g. true/false )
+	
+	local msg = event.response
+	-- just a quick debug message to check what response we got from the library
+	print("Message received from the ads library: ", msg)
+end
+
+return admob
