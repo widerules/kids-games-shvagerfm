01c70a9fe4978816b99d4e61de8def1f28f1f16e
shvagerfm@gmail.com <shvagerfm@gmail.com@b6057328-8ef7-3bbf-bd54-756edf35c897>
2014-04-13 20:37:46 +0000

    Уровни в тенях для формы

---
+++ trunk/Work/ShapesShadows/shapesData.lua
@@ -0,0 +1,19 @@
+local data = {};
+
+data.formPath = "images/"
+data.shapesPath = "images/s"
+data.format = ".png"
+
+data.shapes = 
+{
+	"square",
+	"rectangle",
+	"circle",
+	"oval",
+	"rhombus",
+	"triangle",
+	"star",
+	"heart"
+}
+
+return data;
