b977af6c500769fb90b20d83639700921f4a3fd8
shvagerfm@gmail.com <shvagerfm@gmail.com@b6057328-8ef7-3bbf-bd54-756edf35c897>
2014-04-22 21:09:18 +0000

    Добавлена не до конца обновленная версия локализации фигур на испанском языке.

---
+++ trunk/Publish/Spanish/FormsGameNew/pairData.lua
@@ -0,0 +1,18 @@
+local data = {}
+
+data.foldPath = "images/fold.png"
+data.butterfliesPath = "images/shapes.png"
+data.butterflies = 
+{
+	"square",
+	"triangle",
+	"rhombus",
+	"oval",
+	"rectangle",
+	"circle",
+	"heart",
+	"star"
+}
+data.amount = 12
+
+return data
---
+++ trunk/Publish/Spanish/FormsGameNew/shapesData.lua
@@ -0,0 +1,31 @@
+local data = {};
+
+data.formPath = "images/"
+data.shapesPath = "images/s"
+data.format = ".png"
+
+data.shapes = 
+{
+	"square",
+	"rectangle",
+	"circle",
+	"oval",
+	"rhombus",
+	"triangle",
+	"star",
+	"heart"
+}
+
+data.shapesLocalized =
+{
+    ["square"] = "cuadrado",
+    ["rectangle"] = "rectángulo",
+    ["circle"] = "círculo",
+    ["oval"] = "oval",
+    ["rhombus"] = "romb",
+    ["triangle"] = "triángulo",
+    ["star"] = "estrella",
+    ["heart"] = "corazón"
+}
+
+return data;
