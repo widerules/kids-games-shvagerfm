a730db9f721f3beb9c0bf45a686ab54ebab15879
shvagerfm <shvagerfm@gmail.com>
2014-04-28 14:01:06 +0000

    Animals optimized images (AnimalsNew - копия)

---
+++ Publish/English/AnimalsNew - копия/constants.lua
@@ -0,0 +1,18 @@
+local widget = require("widget");
+
+local consts = {};
+
+consts.W = display.contentWidth;			--storing width
+consts.H = display.contentHeight;			--storing height
+consts.CENTERX = display.contentCenterX;	--storing horizontal center
+consts.CENTERY = display.contentCenterY;	--storing verticall center
+
+--[[consts.homeButton = widget.newButton
+{
+	defaultFile = "images\\home.png",
+	overFile = "images\\home.png",
+	width = 0.1*consts.W;
+	height = 0.1*consts.W;
+}--]]
+
+return consts;
