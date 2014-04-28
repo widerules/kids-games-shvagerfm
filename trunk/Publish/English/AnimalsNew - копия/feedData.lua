769b5b8dc20533f7ded0d905ee3d9922ae769ee4
shvagerfm@gmail.com <shvagerfm@gmail.com@b6057328-8ef7-3bbf-bd54-756edf35c897>
2014-04-28 14:01:06 +0000

    Animals optimized images (AnimalsNew - копия)

---
+++ trunk/Publish/English/AnimalsNew - копия/feedData.lua
@@ -0,0 +1,23 @@
+local data = {}
+
+data.animals = 
+{
+	"bunny",
+	"cat",
+	"cow",
+	"dog",
+	"mouse",
+	"sheep"
+}
+
+data.food =
+{
+	"carrot",
+	"milk",
+	"seno",
+	"bone",
+	"cheese",
+	"grass"	
+}
+
+return data;
