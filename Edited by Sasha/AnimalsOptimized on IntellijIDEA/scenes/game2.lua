aa5b84392ba6d191f4ff3ffdd1ab129dd8729d96
shvagerfm <shvagerfm@gmail.com>
2014-05-13 18:32:05 +0000

    Переделал все сцены с новым popup, чуть-чуть переписал логику работы номера игры, кнопка "Назад" теперь всюду перебрасывает в меню выбора игры (на позицию текущей игры). Портировал проект на IntellijIDEA Перенес explosion в соответсвующие папки Упорядочил файлы data Удалил ненужный модуль popupReload (или как его там)

---
+++ Edited by Sasha/AnimalsOptimized on IntellijIDEA/scenes/game2.lua
@@ -0,0 +1,342 @@
+local storyboard = require( "storyboard")
+local widget = require( "widget")
+local data = require( "data.feedData")
+local constants = require( "constants")
+local popup = require("utils.popup")
+
+local scene = storyboard.newScene( )
+
+local _ANIMALSPATH = "images/"
+local _FOODPATH = "images/"
+local _FORMAT = ".png"
+
+local _FONTSIZE = constants.H / 15;
+local _IMAGESIZE = 0.4*constants.H;
+local _FOODSIZE = 0.7*_IMAGESIZE;
+local _SPACING = 0.1*constants.H;
+local _DELTA = 0.5*_IMAGESIZE;
+
+local _PANECENTERX
+local _BARCENTERX
+
+_GAME = 2
+
+local indexes = {}
+local positions = {}
+local foodPositions = {}
+local animalsPictures = {}
+local foodPictures = {}
+
+local animalSound;
+
+local background, homeBtn
+local rightBar;
+local pane;
+local starToScore
+local counter = 0
+
+explosionTable        = {}                    -- Define a Table to hold the Spawns
+i                    = 0                        -- Explosion counter in table
+explosionTime        = 416.6667                    -- Time defined from EXP Gen 3 tool
+resources            = "utils"            -- Path to external resource files
+explosionImageFolder = "images/explosion"
+local explosionSheetInfo    = require(resources..".".."explosion")
+local explosionSheet        = graphics.newImageSheet( explosionImageFolder.."/".."Explosion.png", explosionSheetInfo:getSheet() )
+
+--------------------------------------------------
+-- Define the animation sequence for the Explosion
+-- from the Sprite sheet data
+-- Change the sequence below to create IMPLOSIONS 
+-- and EXPLOSIONS etc...
+--------------------------------------------------
+local animationSequenceData = {
+  { name = "dbiExplosion",
+      frames={
+          1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
+      },
+      time=explosionTime, loopCount=1
+  },
+}
+
+local function spawnExplosionToTable(spawnX, spawnY)
+    i = i + 1                                        -- Increment the spawn counter
+    
+    explosionTable[i] = display.newSprite( explosionSheet, animationSequenceData )
+    explosionTable[i]:setSequence( "dbiExplosion" )    -- assign the Animation to play
+    explosionTable[i].x=spawnX                        -- Set the X position (touch X)
+    explosionTable[i].y=spawnY                        -- Set the Y position (touch Y)
+    explosionTable[i]:play()                        -- Start the Animation playing
+    explosionTable[i].xScale = 1                    -- X Scale the Explosion if required
+    explosionTable[i].yScale = 1                    -- Y Scale the Explosion if required
+    
+    --Create a function to remove the Explosion - triggered from the DelatedTimer..
+    local function removeExplosionSpawn( object )
+        return function()
+            object:removeSelf()    -- remove the explosion from table
+            object = nil
+        end
+    end
+    
+    --Add a timer to the Spawned Explosion.
+    --Explosion are destroyed after all the frames have been played after a determined
+    --amount of time as setup by the Explosion Generator Tool.
+    local destroySpawneExplosion = timer.performWithDelay (explosionTime, removeExplosionSpawn(explosionTable[i]))
+end
+
+local function backHome( event )
+    --TO DO:
+    local options =
+        {
+            effect = "slideRight",
+            time = 500,
+        }
+    storyboard.gotoScene("scenes.gametitle", options)
+    storyboard.removeScene("scenes.game2")
+
+end;
+
+----animation update score
+local function animScore()
+    local function listener()
+        display.remove( starToScore )
+        starToScore = nil
+    end
+    starToScore = display.newImage( "images/starfull.png", constants.CENTERX, constants.CENTERY, constants.H/8, constants.H/8)
+    starToScore.xScale, starToScore.yScale = 0.1, 0.1
+    
+    local function trans1()
+        transition.to(starToScore, {time = 200, xScale = 1, yScale = 1, x = star[level].x, y= star[level].y, onComplete = listener})
+    end
+    spawnExplosionToTable(constants.CENTERX, constants.CENTERY)
+    transition.to(starToScore, {time = 300, xScale = 2, yScale = 2, transition = easing.outBack, onComplete = trans1})
+end
+
+
+local soundHarp = audio.loadSound( "sounds/harp.ogg")
+
+local onPlaces
+
+local function generateIndexes ()
+        local tmp = {}
+        for i = 1,table.maxn(data.animals),1 do
+                table.insert(tmp,i)
+        end
+
+        local rand;
+        for i = 1, 3, 1 do
+                rand = math.random(1, table.maxn(tmp))
+                table.insert(indexes,tmp[rand])
+                table.remove( tmp, rand )
+        end
+end
+
+local function animScaleOnDrag(self)
+        self.xScale = 1.5
+        self.yScale = 1.5
+end
+
+local function animScaleBack(self)
+        self.xScale = 1
+        self.yScale = 1
+end
+
+local function animOnPutOn(self)
+        local function animFinished()
+                self:toBack()           
+        end
+        transition.scaleTo(self, {xScale = 0, yScale = 0, time = 500, onComplete = animFinished})
+end
+
+local function onHomeButtonClicked(event)
+        --TODO: go to home screen
+        storyboard.gotoScene("scenetemplate", "slideRight", 400)
+        storyboard.removeScene("scenes.game2")
+end
+
+local function onNextButtonClicked(event)
+        storyboard.reloadScene( )
+end
+
+local function onFoodDrag (event)
+local t = event.target
+        local phase = event.phase
+        animScaleOnDrag(t)
+
+        if "began" == phase then 
+                startX = t.x
+                startY = t.y
+                
+                local parent = t.parent
+                parent:insert (t)
+                display.getCurrentStage():setFocus(t)
+                
+                t.isFocus = true
+                t.x0 = event.x - t.x
+                t.y0 = event.y - t.y
+
+        elseif "moved" == phase and t.isFocus then
+                t.x = event.x-t.x0
+                t.y = event.y-t.y0
+        elseif "ended" == phase or "cancel" == phase then 
+                
+                local flag = false
+                local index = 0
+
+                --find which food dragged
+                for i = 1, table.maxn(foodPictures), 1 do
+                        if foodPictures[i]==t then
+                                index = i
+                        end
+                end             
+
+                if math.abs(t.x - animalsPictures[index].x)<_DELTA and math.abs (t.y-animalsPictures[index].y)<_DELTA then
+                        t.x = animalsPictures[index].x
+                        t.y = animalsPictures[index].y
+                        local name = tostring(animalsPictures[index].name)
+                        animalSound = audio.loadSound("sounds/"..name..".mp3")
+                        onPlaces = onPlaces + 1;
+                        animOnPutOn(t)
+                        audio.play(animalSound)
+                        spawnExplosionToTable(t.x, t.y)
+                        t:removeEventListener( "touch", onFoodDrag )
+                else 
+                        t.x = startX
+                        t.y = startY
+                        animScaleBack(t)
+                end;
+
+                display.getCurrentStage():setFocus(nil)
+                t.isFocus = false
+                startX = nil
+                startY = nil
+
+                if onPlaces == 3 then
+                		audio.play( soundHarp )
+                        timer.performWithDelay( 800, popup.showPopupWithReloadButton("You won !", "scenes.gametitle", "scenes.game2"), 1)
+                end
+
+        end
+end
+
+function scene:createScene(event)
+        local group = self.view
+        local feedSound = audio.loadSound( "sounds/feed.mp3")
+        audio.play( feedSound )
+        background = display.newImage("images/background3.jpg", constants.CENTERX, constants.CENTERY)
+        background.height = constants.H
+        background.width = constants.W
+        group:insert(background)
+
+        rightBar = display.newImage("images/right_bar.png", 0, constants.CENTERY)
+        rightBar.height = constants.H
+        rightBar.width = 0.2*constants.W
+        rightBar.x = constants.W - rightBar.width/2;
+        group:insert(rightBar)
+
+        _BARCENTERX = rightBar.x
+
+        pane = display.newImage("images/layer.png",0,constants.CENTERY)
+        pane.height = 0.9*constants.H
+        pane.width = 0.7*constants.W
+        pane.x = pane.width/2+0.05*constants.W
+        group:insert(pane)
+
+        _PANECENTERX = pane.x
+
+        positions = 
+        {
+                x = 
+                {
+                        pane.x - _IMAGESIZE/2-_SPACING,
+                        pane.x + _IMAGESIZE/2+_SPACING,
+                        pane.x - _IMAGESIZE/2-_SPACING
+                },
+                y = 
+                {
+                        pane.y - _IMAGESIZE/2 - _SPACING/3,
+                        pane.y,
+                        pane.y + _IMAGESIZE/2 + _SPACING/3
+
+                }       
+        }
+
+    backBtn = widget.newButton
+        {
+            height = 0.07*constants.W,
+            width = 0.07*constants.W,
+            left = 0,
+            top = 0,
+            defaultFile = "images/home.png",
+            overFile = "images/homehover.png",
+            id = "home",
+            onRelease = backHome,
+            
+        }
+    group:insert( backBtn )
+
+end
+
+function scene:enterScene(event)
+        local group = self.view;
+
+        onPlaces = 0
+
+        generateIndexes();
+        positions.foodY = 
+        {
+                _FOODSIZE/2+_SPACING,
+                3*_FOODSIZE/2+_SPACING,
+                5*_FOODSIZE/2+_SPACING
+        }
+
+        local randFood
+
+        for i = 1, 3, 1 do
+                animalsPictures[i] = display.newImage( _ANIMALSPATH..data.animals[indexes[i]].._FORMAT, 0, 0)
+                animalsPictures[i].height = _IMAGESIZE
+                animalsPictures[i].width = _IMAGESIZE
+                animalsPictures[i].name = data.animals[indexes[i]]
+                group:insert(animalsPictures[i])
+                transition.to(animalsPictures[i], {x=positions.x[i], y=positions.y[i], transition=easing.outBounce, time = 400})
+
+                randFood = math.random( 1, table.maxn(positions.foodY))
+
+                foodPictures[i] = display.newImage (_FOODPATH..data.food[indexes[i]].._FORMAT, constants.W, positions.foodY[randFood])
+                foodPictures[i].height = _FOODSIZE
+                foodPictures[i].width = _FOODSIZE
+                foodPictures[i]:addEventListener( "touch", onFoodDrag )
+                group:insert(foodPictures[i])   
+                transition.to(foodPictures[i], {x=_BARCENTERX, transition=easing.outBounce, time = 400})
+                table.remove(positions.foodY, randFood)
+        end
+end
+
+function scene:exitScene(event)
+        while (table.maxn(indexes)>0) do
+                table.remove( indexes )
+        end
+
+        while (table.maxn(animalsPictures) > 0) do
+            display.remove( animalsPictures[#animalsPictures] )
+                table.remove(animalsPictures)
+        end
+
+        while (table.maxn(foodPictures) > 0) do
+            display.remove( foodPictures[#foodPictures] )              
+                table.remove(foodPictures)
+        end
+
+            display.remove( nextBtn )
+            display.remove( homeBtn )
+
+end
+
+function scene:destroyScene(event)
+end
+
+scene:addEventListener( "createScene", scene )
+scene:addEventListener( "enterScene", scene )
+scene:addEventListener( "exitScene", scene )
+scene:addEventListener( "destroyScene", scene )
+
+return scene
---
+++ Edited by Sasha/AnimalsOptimized on IntellijIDEA/scenes/game4.lua
@@ -0,0 +1,405 @@
+local storyboard = require( "storyboard")
+local widget = require("widget")
+local popup = require("utils.popup")
+
+local data = require("data.itemsData")
+local constants = require( "constants")
+
+local scene = storyboard.newScene()
+
+local _BERRYPATH = "images/berry.png"
+local _HOGPATH = "images/hog.png"
+local _MUSHPATH = "images/grib"
+local _FORMAT = ".png"
+
+local _FONTSIZE = constants.H / 15
+local _IMAGESIZE = 0.2*constants.H
+
+_GAME = 4
+
+local iteration = 1
+local itemsFound = 0
+
+local score
+local sun
+local cloud
+local rain
+
+local group
+
+local layers = {}
+local groups = {}
+local hogs = {}
+local mushrooms ={}
+local berries = {}
+
+local soundHarp 
+local birdSound
+local rainSound
+local hogSound
+
+local timers = {}
+local total, totalScore, bgscore, coins
+local coinsToScore
+---------------------------------------------
+--explosion
+--------------------------------------------------
+explosionTable        = {}                    -- Define a Table to hold the Spawns
+i                    = 0                        -- Explosion counter in table
+explosionTime        = 416.6667                    -- Time defined from EXP Gen 3 tool
+resources            = "utils"            -- Path to external resource files
+explosionImageFolder = "images/explosion"
+
+local explosionSheetInfo    = require(resources..".".."explosion")
+local explosionSheet        = graphics.newImageSheet( explosionImageFolder.."/".."Explosion.png", explosionSheetInfo:getSheet() )
+
+--------------------------------------------------
+-- Define the animation sequence for the Explosion
+-- from the Sprite sheet data
+-- Change the sequence below to create IMPLOSIONS 
+-- and EXPLOSIONS etc...
+--------------------------------------------------
+local animationSequenceData = {
+  { name = "dbiExplosion",
+      frames={
+          1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
+      },
+      time=explosionTime, loopCount=1
+  },
+}
+
+local function spawnExplosionToTable(spawnX, spawnY)
+    i = i + 1                                        -- Increment the spawn counter
+    
+    explosionTable[i] = display.newSprite( explosionSheet, animationSequenceData )
+    explosionTable[i]:setSequence( "dbiExplosion" )    -- assign the Animation to play
+    explosionTable[i].x=spawnX                        -- Set the X position (touch X)
+    explosionTable[i].y=spawnY                        -- Set the Y position (touch Y)
+    explosionTable[i]:play()                        -- Start the Animation playing
+    explosionTable[i].xScale = 1                    -- X Scale the Explosion if required
+    explosionTable[i].yScale = 1                    -- Y Scale the Explosion if required
+    
+    --Create a function to remove the Explosion - triggered from the DelatedTimer..
+    local function removeExplosionSpawn( object )
+        return function()
+            object:removeSelf()    -- remove the explosion from table
+            object = nil
+        end
+    end
+    
+    --Add a timer to the Spawned Explosion.
+    --Explosion are destroyed after all the frames have been played after a determined
+    --amount of time as setup by the Explosion Generator Tool.
+    local destroySpawneExplosion = timer.performWithDelay (explosionTime, removeExplosionSpawn(explosionTable[i]))
+end
+
+
+local function onNextButtonClicked()
+	storyboard.reloadScene( )
+end
+
+local function onHomeButtonClicked( event )
+	--TO DO:
+	local options =
+		{
+    		effect = "slideRight",
+    		time = 400,
+    		params = { ind = 4 }
+		}
+	storyboard.gotoScene("scenes.gametitle", options)
+	storyboard.removeScene("scenes.game4")
+
+end;
+
+local function onItemClicked(event)
+	local t = event.target
+
+	local function updateScore ()
+		score.text = "Score: "..itemsFound
+	end
+
+	local function vanishAway ()    
+		transition.to(t,{time = 1000, alpha = 0, x =constants.W, y = 0, xScale = 0.1, yScale = 0.1})
+	end
+	transition.scaleTo(t, {xScale = 1.5*t.xScale, yScale = 1.5*t.yScale, time = 500, onComplete = vanishAway})
+	spawnExplosionToTable(t.x, t.y)
+	t:removeEventListener( "touch", onItemClicked ) 
+
+	itemsFound = itemsFound + 1
+	
+	if (itemsFound == 7) then
+		soundHarp = audio.loadSound( "sounds/harp.ogg")
+		audio.play( soundHarp )
+		timers[#timers+1] = timer.performWithDelay( 800, popup.showPopupWithNextButton("Well done !", "scenes.gametitle", "scenes.game4"), 1)
+	end
+end
+
+local function fillWithHogs(group)
+	local i
+	hogSound = audio.loadSound("sounds/hog_sound2.mp3")
+	audio.play(hogSound)
+
+	for i = 1, 7, 1 do
+		local function addHog()
+			hogs[i] = display.newImage (_HOGPATH, data.hogsPositions.x[i], data.hogsPositions.y[i])
+			hogs[i].width = data.HOGIMAGESIZE*data.hogsSizes[i]
+			hogs[i].height = data.HOGIMAGESIZE*data.hogsSizes[i]
+			hogs[i].xScale = 0.3
+			hogs[i]:addEventListener( "touch", onItemClicked )  
+			transition.to(hogs[i], {xScale = data.hogsScale[i], alpha =1, transition=easing.outBack, time = 300})                    
+			groups[data.hogsGroups[i]]:insert (hogs[i])                             
+		end
+		timers[#timers + 1] = timer.performWithDelay( i*300, addHog )
+	end  
+	local function sayFind()
+		findSound = audio.loadSound( "sounds/findhedgehogs.mp3" )
+		audio.play( findSound )
+	end
+	timer.performWithDelay( 2100, sayFind )   
+end
+
+local function fillWithMushrooms(group)
+	local i
+	for i = 1, 7, 1 do
+		local function addMushroom()
+			mushrooms[i] = display.newImage (_MUSHPATH..data.mushroomsTypes[i].._FORMAT, data.mushroomsPositions.x[i], data.mushroomsPositions.y[i])
+			mushrooms[i].width = data.MUSHIMAGESIZE*data.mushroomsSizes[i]
+			mushrooms[i].height = data.MUSHIMAGESIZE*data.mushroomsSizes[i]
+			mushrooms[i].xScale = 0.3
+			mushrooms[i]:addEventListener( "touch", onItemClicked )
+			transition.to(mushrooms[i], {xScale = data.mushroomsScale[i], alpha =1, transition=easing.outBack, time = 300})
+			groups[data.mushroomsGroups[i]]:insert(mushrooms[i])
+		end
+		timers[#timers + 1] = timer.performWithDelay( i*300, addMushroom)
+	end
+	local function sayFind()
+		findSound = audio.loadSound( "sounds/findmushrums.mp3" )
+		audio.play( findSound )
+	end
+	timer.performWithDelay( 2100, sayFind ) 
+end
+
+local function fillWithBerries(group)
+	local i
+	for i = 1, 7, 1 do
+		local function addBerry()
+				berries[i] = display.newImage(_BERRYPATH, data.berriesPosition.x[i], data.berriesPosition.y[i])
+				berries[i].width = data.BERRYIMAGESIZE * data.berriesSizes[i]
+				berries[i].height = data.BERRYIMAGESIZE * data.berriesSizes[i]
+				transition.to(mushrooms[i], {xScale = 1, alpha =1, transition=easing.outBack, time = 300})
+				berries[i]:addEventListener( "touch", onItemClicked )
+				groups[data.berriesGroups[i]]:insert(berries[i])
+		end
+		timers[#timers + 1] = timer.performWithDelay( i*300, addBerry )
+	end
+	local function sayFind()
+		findSound = audio.loadSound( "sounds/findberryes.mp3" )
+		audio.play( findSound )
+	end
+	timer.performWithDelay( 2100, sayFind ) 
+end
+
+local function sunAnimation ()
+	birdSound = audio.loadSound("sounds/birds_sing.ogg")
+	audio.play(birdSound)
+
+	sun = display.newImage("images/sun.png", constants.W, constants.H)
+	sun.width = _IMAGESIZE 
+	sun.height = _IMAGESIZE
+	sun.anchorY = 0.4
+	
+	group:insert(sun)
+	local function fill ()
+		audio.stop()
+		timers[#timers + 1] = timer.performWithDelay( 500, fillWithHogs )
+	end
+
+	local function onRotateNeg()
+		transition.to(sun, {rotation = 0, time = 1000, onComplete = fill})              
+	end
+
+	local function onRotatePos()            
+		transition.to(sun, {rotation = 360, time = 1000, onComplete = onRotateNeg})                     
+	end
+
+	local function toNormal ()
+		transition.scaleTo(sun, {xScale = 1, yScale = 1, time = 500, onComplete = onRotatePos})
+	end
+
+	local function toBig()
+		transition.scaleTo(sun, {xScale = 1.5, yScale = 1.5, time = 500, onComplete = toNormal})
+	end
+
+	transition.moveTo(sun, {x = constants.CENTERX, y = _IMAGESIZE*0.8, time = 2000, transition=easing.outBack, onComplete = toBig})
+end
+
+local function onRainFinished(event)
+	if (event.phase == "ended") then
+		if (rain~=nil) then
+			display.remove( rain )
+			rain = nil
+		end
+		audio.pause( rainSound )
+		fillWithMushrooms()
+	end
+end
+
+
+local function rainAnimation()
+	rainSound = audio.loadSound("sounds/rain_sound.ogg")
+	audio.play( rainSound )
+	cloud = display.newImage("images/tucha.png", 0,0)
+	cloud.width = _IMAGESIZE
+	cloud.height = _IMAGESIZE
+	
+	group:insert(cloud)
+
+	local function rainStart()
+		local rainSheetData = 
+		{
+			width = constants.H,
+			height = 2*constants.H/3,
+			numFrames = 4,
+			sheetContentWidth = constants.H*2,
+			sheetContentHeight = 4*constants.H/3
+		}
+		local rainSheet = graphics.newImageSheet("images/rain.png", rainSheetData)
+		local sequenceDataRain = 
+		{
+			name = "rain",
+			start = 1,
+			count = 4,
+			time = 100,
+			loopCount=6,
+			loopDirection = forward
+		}
+		
+		rain = display.newSprite( rainSheet, sequenceDataRain)
+		group:insert(rain)
+		rain.x = constants.CENTERX
+		rain.y = constants.CENTERY
+		rain:addEventListener( "sprite", onRainFinished)
+		rain.timeScale = 0.4
+		rain:play()
+
+	end
+	transition.moveTo(cloud, {x = constants.CENTERX, y = _IMAGESIZE*0.8, time = 2000, onComplete = rainStart})
+end
+
+function scene:createScene (event)
+
+	group = self.view
+	local i
+
+	for i = 1, 10, 1 do
+		layers[i] = display.newImage("images/layer"..i..".png", constants.CENTERX, constants.CENTERY)
+		layers[i].height = constants.H
+		layers[i].width = constants.W
+	end     
+
+	for i = 1,9,1 do
+		groups[i] = display.newGroup( )
+	end
+	
+	group:insert (1, layers[1])
+	group:insert (2, groups[1])
+	group:insert (3, layers[2])
+	group:insert (4, groups[2])
+	group:insert (5, layers[3])
+	group:insert (6, groups[3])
+	group:insert (7, layers[4])
+	group:insert (8, groups[4])
+	group:insert (9, layers[5])
+	
+	group:insert (10, groups[5])
+	group:insert (11, layers[6])
+	group:insert (12, groups[6])
+	group:insert (13, layers[7])
+
+	group:insert (14, groups[7])
+	group:insert (15, layers[8])
+	group:insert (16, groups[8])
+	group:insert (17, layers[9])
+	group:insert (18, groups[9])
+	group:insert (19, layers[10])
+	
+end
+		
+function scene:enterScene(event)
+	itemsFound = 0
+
+	if iteration == 1 then  
+		
+		sunAnimation()                  
+		iteration = iteration + 1               
+	elseif iteration == 2 then
+		
+		rainAnimation()
+		
+		iteration = iteration + 1
+	else
+		
+		fillWithBerries(group)
+		iteration = 1
+	end
+end
+
+function scene:exitScene(event)
+	
+	audio.stop()
+	audio.dispose( rainSound )
+	rainSound = nil
+	audio.dispose(soundHarp)
+	soundHarp = nil
+	audio.dispose(birdSound)
+	birdSound = nil
+	audio.dispose(hogSound)
+	hogSound = nil
+
+	while (#timers > 0) do
+		timer.cancel(timers[#timers])
+		table.remove( timers )
+	end
+
+	transition.cancel()
+	
+	if (rain~=nil) then
+		display.remove( rain )
+		rain = nil
+	end
+
+	while (table.maxn(hogs)>0) do
+		display.remove( hogs[#hogs] )
+        table.remove( hogs )
+	end
+
+	while (table.maxn(mushrooms)>0) do
+		display.remove( mushrooms[#mushrooms] )
+        table.remove(mushrooms)
+	end
+
+	while (table.maxn(berries)>0) do
+		display.remove( berries[#berries] )
+        table.remove(berries)
+	end
+
+	if (sun ~= nil) then
+		display.remove( sun )
+        sun = nil
+	end
+
+	if (cloud ~= nil) then
+		display.remove( cloud )
+        cloud = nil
+	end
+end
+
+function scene:destroyScene(event)
+end
+
+scene:addEventListener( "createScene", scene )
+scene:addEventListener( "enterScene", scene )
+scene:addEventListener( "exitScene", scene )
+scene:addEventListener( "destroyScene", scene )
+
+return scene
