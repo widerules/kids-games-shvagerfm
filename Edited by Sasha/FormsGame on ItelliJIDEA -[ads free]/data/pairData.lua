fed405d2e15e9c7721619a534405102f2d09cda5
shvagerfm <shvagerfm@gmail.com>
2014-05-26 14:53:53 +0000

    FormsGame on ItelliJIDEA: добавлена кнопка Diable ads, ведущая на платную версию приложение в маркете Добавлена FormsGame on ItelliJIDEA -[ads free]: платная версия игры без рекламы (убран рекламный модуль admobe)

---
+++ Edited by Sasha/FormsGame on ItelliJIDEA -[ads free]/data/pairData.lua
@@ -0,0 +1,24 @@
+local data = {};
+
+data.animalsPath = "images/"
+data.format = ".png"
+data.foldPath = "images/fold.png"
+data.animals = 
+{
+	"square",
+	"triangle",
+	"rhombus",
+	"oval",
+	"rectangle",
+	"trapezoid",
+	"pentagon",
+	"hexagon",
+	"circle",
+	"semicircle",
+	"parallelogram",
+	"heart",
+	"star"
+}
+
+
+return data
