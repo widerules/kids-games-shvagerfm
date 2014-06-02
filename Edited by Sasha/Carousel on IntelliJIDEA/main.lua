a8d55850820e6e9cd22662723720831d0519e107
shvagerfm <shvagerfm@gmail.com>
2014-06-02 17:15:27 +0000

    Добавил карусель

--- Edited by Sasha/Carousel on IntelliJIDEA/main.lua
+++ Edited by Sasha/Carousel on IntelliJIDEA/main.lua
@@ -1,27 +1,81 @@
 local memoryViewer = require ("memoryViewer")
+local widget = require("widget")
 local data = require("data")
 local slide = require("slide")
+local slideObject = require("slideObject")
+local carousel = require( "carousel" )
 
 shouldWork = true
-
 memoryViewer.create(display.contentCenterX, 20, shouldWork)
 memoryViewer.updateInfoInLoop(100)
 
-local firstSlide = slide:create( display.contentCenterX, display.contentCenterY, 500, 500 )
-firstSlide:insertObject( data.showLabel, data.hideLabel, "Label" )
-firstSlide:insertObject( data.showLongLabel, data.hideLongLabel, "longLabel" )
-firstSlide:insertObject( data.showImage, data.hideImage, "Image" )
+local myCarousel
+local myImages = {} -- массив картинок, которые будут отображаться на слайде
+local myLabels = {} -- массив надписей, которые будут отображаться на слайде
 
-firstSlide:showObject( 1 )
-firstSlide:showObject( "longLabel" )
-firstSlide:showObject( 3 )
+-- кнопка для отображения следующего слайда
+local next = widget.newButton
+    {
+        x = display.contentCenterX + display.contentCenterX / 4,
+        y = display.contentCenterX,
+        defaultFile = "btn.png",
+        overFile = "btn.png",
+        label = "Next",
+        fontSize = 20,
+        labelColor = { default={ 0, 0, 0 }, over={ 0, 0, 0, 0.5 } },
+        onRelease = function ()
+            myCarousel:showNextSlide()
+        end
+    }
 
-print( firstSlide:getObjectsCount() )
+-- кнопка для отображения предыдущего слайда
+local previous = widget.newButton
+    {
+        x = display.contentCenterX / 4,
+        y = display.contentCenterX,
+        defaultFile = "btn.png",
+        overFile = "btn.png",
+        label = "Prev",
+        fontSize = 20,
+        labelColor = { default={ 0, 0, 0 }, over={ 0, 0, 0, 0.5 } },
+        onRelease = function ()
+            myCarousel:showPreviousSlide()
+        end
+    }
 
-local function hideAll()
-    firstSlide:hideObject( "Label" )
-    firstSlide:hideObject( 2 )
-    firstSlide:hideObject( "Image" )
+-- создаем карусель по центру экрана размером 500х500
+myCarousel = carousel.new( display.contentCenterX, display.contentCenterY, 500, 500 )
+
+-- цикл добавления слайдов с объектами в карусель
+for i = 1, 4 do
+
+    -- создаем новый объект слайда (изображение) и вносим его в массив
+    myImages[i] = slideObject.new()
+    myImages[i].name = "image"..i
+    myImages[i].createFunction = function( id ) myImages[id] = display.newImage( id..".png", id*30, id*30 ) return myImages[id] end
+    myImages[i].showFunction = function( id ) myImages[id].isVisible = true print("id in show im: "..id) end
+    myImages[i].hideFunction =  function( id ) myImages[id].isVisible = false  end
+    myImages[i].removeFunction = function( id ) display.remove( myImages[id] ) myImages[id] = nil end
+
+    -- создаем новый объект слайда (надпись) и вносим его в массив
+    myLabels[i] = slideObject.new()
+    myLabels[i].name = "label"..i
+    myLabels[i].createFunction = function( id ) myLabels[id] = display.newText( "Label"..id, id*5, id*5, native.systemFont, 46 ) return myLabels[id] end
+    myLabels[i].showFunction = function( id ) myLabels[id].isVisible = true  print("id in show lab: "..id) end
+    myLabels[i].hideFunction =  function( id ) myLabels[id].isVisible = false end
+    myLabels[i].removeFunction = function( id ) display.remove(myLabels[id]) myLabels[id] = nil end
+
+
+    -- создать слайд по центру экрана размером 200х200 с именем slide (i-тый)
+    local myNewSlide = slide.new( display.contentCenterX, display.contentCenterY, 200, 200, "slide"..i )
+
+    -- добавить объекты из массивов на слайд
+    myNewSlide:insertObject( myLabels[i] )
+    myNewSlide:insertObject( myImages[i] )
+
+    -- добавить слайд в карусель
+    myCarousel:insertSlide(  myNewSlide )
 end
 
-timer.performWithDelay( 4000, hideAll)
+
+myCarousel:showSlide( 1 ) -- показать первый слайд
--- Edited by Sasha/Carousel on IntelliJIDEA/slide.lua
+++ Edited by Sasha/Carousel on IntelliJIDEA/slide.lua
@@ -1,70 +1,79 @@
 local slide = {}
-
 local slideMetaTable = { __index = slide }
 
-local slideData = {
-    objectsNames = {},
-    showingFunctions = {},
-    hidingFunctions = {},
-    x = 0, y = 0,
-    width = 0,
-    height = 0,
-    objectsCount = 0,
-    viewGroup = nil
-}
+function slide.new( x, y, width, height, name )
 
-local function invokeShowingFunction( func )
-    local object = func()
-    slideData.viewGroup:insert(object)
-end
+    local slideData = {
+        x = nil,
+        y = nil,
+        width = nil,
+        height = nil,
+        objectsCount = nil,
+        objects = {},
+        viewGroup = nil,
+        name = nil
+    }
 
-local function invokeHidingFunction( func )
-    func()
-end
-
-slide.create = function( x, y, width, height )
     slideData.objectsCount = 0
-
     slideData.x = x
     slideData.y = y
-
     slideData.viewGroup = display.newGroup()
-	
-	if widthValue>0 then
+    slideData.name = name
+
+    if width>0 then
         slideData.width = width
-	else
-		print("width == " .. width .. ". It should be greater the 0!")
+    else
+        print("width == " .. width .. ". It should be greater the 0!")
     end
-		
-	if heightValue>0 then
+
+    if height>0 then
         slideData.height = height
-	else
-		print("height == " .. height .. ". It should be greater the 0!")
+    else
+        print("height == " .. height .. ". It should be greater the 0!")
     end
 
     return setmetatable( slideData, slideMetaTable )
 end
 
-function slide:getObjectsCount()
-	return self.objectsCount
+function slide:invokeShowingFunctionForObject( objectIdentificator, slideId )
+
+    -- create object if needed
+    if self.objects[objectIdentificator].object == nil then
+        self.objects[objectIdentificator].object = self.objects[objectIdentificator].createFunction( slideId )
+        self.objects[objectIdentificator].object.realX = self.objects[objectIdentificator].object.x
+        self.objects[objectIdentificator].object.realY = self.objects[objectIdentificator].object.y
+        self.viewGroup:insert(self.objects[objectIdentificator].object) -- add object to viewGroup
+    end
+
+    -- calculate object possition
+    local objectX = self.objects[objectIdentificator].object.realX
+    local objectY = self.objects[objectIdentificator].object.realY
+    self.objects[objectIdentificator].object.x = (self.x - self.width * 0.5) + objectX
+    self.objects[objectIdentificator].object.y = (self.y - self.height * 0.5) + objectY
+
+    self.objects[objectIdentificator].showFunction( slideId ) -- show object
 end
 
-function slide:insertObject( showingFunction, hidingFunction, objectName )
-	if showingFunction == nil or hidingFunction == nil then
-		print("One of the functions is not exist!")
-        return false
-	else
-		self.showingFunctions[self.objectsCount + 1] = showingFunction
-		self.hidingFunctions[self.objectsCount + 1] = hidingFunction
+function slide:invokeHidingFunctionForObject( objectIdentificator, slideId )
+    print("hidding object: "..objectIdentificator)
+    self.objects[objectIdentificator].hideFunction( slideId ) -- hide object
+end
 
-        if objectName ~= nil then
-            self.objectsNames[self.objectsCount + 1] = objectName;
-        end
+function slide:invokeRemovingFunctionForObject( objectIdentificator, slideId )
+    self.objects[objectIdentificator].removeFunction( slideId )
+    self.objects[objectIdentificator].object = nil
+end
 
-        self.objectsCount = self.objectsCount + 1
+function slide:count()
+    return self.objectsCount
+end
 
-        return true
-	end
+function slide:insertObject( object )
+
+    self.objectsCount = self.objectsCount + 1
+    self.objects[self.objectsCount] = object
+
+    return true
 end
 
 function slide:showObject( identificator )
@@ -72,18 +81,18 @@ function slide:showObject( identificator )
     -- show by number
     if type(identificator) == "number" then
         if identificator <= self.objectsCount and identificator>0 then
-            invokeShowingFunction(self.showingFunctions[identificator])
+            self:invokeShowingFunctionForObject(identificator)
             return true
         else
             print("Wrong number of object!")
         end
 
-    -- show by object name
+        -- show by object name
     elseif type(identificator) == "string" then
         local i
         for i = 1, self.objectsCount do
-            if self.objectsNames[i] == identificator then
-                invokeShowingFunction(self.showingFunctions[i])
+            if self.objects[i].name == identificator then
+                self:invokeShowingFunctionForObject(i)
                 return true
             end
         end
@@ -99,18 +108,18 @@ function slide:hideObject( identificator )
     -- hide by number
     if type(identificator) == "number" then
         if identificator <= self.objectsCount and identificator>0 then
-            invokeHidingFunction(self.hidingFunctions[identificator])
+            self:invokeHidingFunctionForObject(identificator)
             return true
         else
             print("Wrong number of object!")
         end
 
-    -- hide by object name
+        -- hide by object name
     elseif type(identificator) == "string" then
         local i
         for i = 1, self.objectsCount do
-            if self.objectsNames[i] == identificator then
-                invokeHidingFunction(self.hidingFunctions[i])
+            if self.objects[i].name == identificator then
+                self:invokeHidingFunctionForObject(i)
                 return true
             end
         end
@@ -121,27 +130,51 @@ function slide:hideObject( identificator )
     end
 end
 
-function slide:showAllObjects()
+function slide:removeObject( identificator )
+
+    -- remove by number
+    if type(identificator) == "number" then
+        if identificator <= self.objectsCount and identificator>0 then
+            self:invokeRemovingFunctionForObject(identificator)
+            return true
+        else
+            print("Wrong object identificator!")
+        end
+
+        -- remove by name
+    elseif type(identificator) == "string" then
+        local i
+        for i = 1, self.objectsCount do
+            if self.objects[i].name == identificator then
+                self:invokeRemovingFunctionForObject(i)
+                return true
+            end
+        end
+    else
+        print("Wrong object identificator!")
+    end
+
+    return false
+end
+
+function slide:showAllObjects( slideId )
     local i
     for i = 1, self.objectsCount do
-        invokeShowingFunction(self.showingFunctions[i])
+        self:invokeShowingFunctionForObject(i, slideId)
     end
 end
 
-function slide:hideAllObjects()
+function slide:hideAllObjects( slideId )
     local i
     for i = 1, self.objectsCount do
-        invokeHidingFunction(self.hidingFunctions[i])
+        self:invokeHidingFunctionForObject(i, slideId)
     end
 end
 
-function slide:destroy()
+function slide:destroy( slideId )
     local i
     for i = 1, self.objectsCount do
-        invokeHidingFunction(self.hidingFunctions[i])
-        self.hidingFunctions[i] = nil
-        self.showingFunctions[i] = nil
-        self.objectsNames[i] = nil
+        self:invokeRemovingFunctionForObject(i, slideId)
     end
 end
 
