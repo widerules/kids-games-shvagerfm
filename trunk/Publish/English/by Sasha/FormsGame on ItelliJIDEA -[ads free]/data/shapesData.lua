42103a7f71b4e48d36962e17e11fcf3a03ca7820
shvagerfm@gmail.com <shvagerfm@gmail.com@b6057328-8ef7-3bbf-bd54-756edf35c897>
2014-06-06 18:06:47 +0000

    AN-15: заменил модули взрыва на аналогичные из ColorsForKidsOptimized on IntelliJIDEA

---
+++ trunk/Publish/English/by Sasha/FormsGame on ItelliJIDEA -[ads free]/data/shapesData.lua
@@ -0,0 +1,24 @@
+local data = {};
+
+data.formPath = "images/"
+data.shapesPath = "images/s"
+data.format = ".png"
+
+data.shapes = 
+{
+	"square",
+	"rectangle",
+	"circle",
+	"oval",
+	"rhombus",
+	"triangle",
+	"trapezoid",
+	"pentagon",
+	"hexagon",
+	"star",
+	"heart",
+	"semicircle",
+	"parallelogram"
+}
+
+return data;
---
+++ trunk/Publish/English/by Sasha/FormsGame on ItelliJIDEA/main.lua
@@ -0,0 +1,62 @@
+local storyboard = require "storyboard"
+local rate = require( "utils.rate" )
+local memoryViewer = require( "utils.memoryViewer" )
+local constants = require( "constants" )
+
+_SOUNDON = true
+
+storyboard.purgeOnSceneChange = true
+
+shouldWork = true
+
+memoryViewer.create(constants.W/2, 20, shouldWork)
+memoryViewer.updateInfoInLoop(100)
+
+local function exit ()
+  rate.init()
+end
+local function onKeyEvent( event )
+
+   local phase = event.phase
+   local keyName = event.keyName
+  
+
+   if ( ("back" == keyName or "deleteBack" == keyName) and phase == "up" ) then
+    local currentScene = storyboard.getCurrentSceneName()
+     local lastScene = storyboard.getPrevious()
+
+            
+            if ( currentScene == "scenes.scenetemplate") then
+               exit()
+          
+            else
+                timer.performWithDelay(500, function()
+                    transition.cancel( )
+                    storyboard.gotoScene( "scenes.scenetemplate" )
+                    storyboard.removeAll()
+                end)
+            end
+   
+   end
+   if ( keyName == "volumeUp" and phase == "down" ) then
+      local masterVolume = audio.getVolume()
+      if ( masterVolume < 1.0 ) then
+         masterVolume = masterVolume + 0.1
+         audio.setVolume( masterVolume )
+      end
+      return false
+   elseif ( keyName == "volumeDown" and phase == "down" ) then
+      local masterVolume = audio.getVolume()
+      if ( masterVolume > 0.0 ) then
+         masterVolume = masterVolume - 0.1
+         audio.setVolume( masterVolume )
+      end
+      return false
+   end
+   return true  --SEE NOTE BELOW
+end
+Runtime:addEventListener( "key", onKeyEvent )
+-- load scenetemplate.lua
+storyboard.gotoScene( "scenes.scenetemplate" )
+
+-- Add any objects that should appear on all scenes below (e.g. tab bar, hud, etc.):
