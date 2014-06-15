650c50c9410ffdeb60310018c5e3c520daf8c3d3
shvagerfm@gmail.com <shvagerfm@gmail.com@b6057328-8ef7-3bbf-bd54-756edf35c897>
2014-06-15 15:25:01 +0000

    Добавил утилиту Coordinates.

---
+++ trunk/Edited by Sasha/Coordinates/config.lua
@@ -0,0 +1,43 @@
+if ( display.pixelHeight / display.pixelWidth > 1.7) then
+    application =
+    {
+        content =
+        {
+            width = 720,
+            height = 1280,
+            scale = "letterBox",
+            xAlign = "center",
+            yAlign = "center",
+
+        },
+    }
+
+elseif (display.pixelHeight / display.pixelWidth > 1.6) then
+    application =
+    {
+        content =
+        {
+            width = 480,
+            height = 800,
+            scale = "letterBox",
+            xAlign = "center",
+            yAlign = "center",
+
+        },
+    }
+elseif (display.pixelHeight / display.pixelWidth < 1.4) then
+    application =
+    {
+        content =
+        {
+            width = 1024,
+            height = 768,
+            scale = "letterBox",
+            xAlign = "center",
+            yAlign = "center",
+
+        },
+    }
+
+end
+
---
+++ trunk/Edited by Sasha/Coordinates/main.lua
@@ -0,0 +1,6 @@
+local storyboard = require "storyboard"
+
+storyboard.prevScene = 0
+storyboard.purgeOnSceneChange = true
+
+storyboard.gotoScene( "work" )
