f9610a04467f2c79cef45db4f9df208ae1dd0f47
shvagerfm@gmail.com <shvagerfm@gmail.com@b6057328-8ef7-3bbf-bd54-756edf35c897>
2014-06-21 16:39:49 +0000

    PUZ-14: реализовал модуль puzzleGame.lua

--- trunk/Edited by Sasha/Education/work with data base/main.lua
+++ trunk/Edited by Sasha/Education/work with data base/main.lua
@@ -1,24 +1,7 @@
 local database = require("database")
+local composer = require("composer")
 
 database.create() -- create empty database
 database.fill() -- fill database
 
--- print parts info
-local parts = database.getPuzzleParts("puzzle_name_1") -- get array of Part for some puzzle
-for i = 1, #parts do
-    print(parts[i].x, parts[i].y, parts[i].picture)
-end
-
--- print levels names
-local levels = database.getLocationLevels("Forest")
-for i = 1, #levels do
-    print(levels[i].name)
-end
-
--- print level puzzle(s)
-local levelPuzzles = database.getLevelPuzzles(1)
-for i = 1, #levelPuzzles do
-    print(levelPuzzles[i].name)
-end
-
-database.close() -- close database
+composer:gotoScene("work")
